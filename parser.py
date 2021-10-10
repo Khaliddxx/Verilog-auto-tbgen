7ad937710c2ba2b4455834817c485a806fd367e6
Aly Zidan <Aly@Alys-MacBook-Pro.local>
2021-10-10 20:47:16 +0200

    hi

--- parser.py
+++ parser.py
@@ -3,13 +3,13 @@ import hdlparse.verilog_parser as vlog
 
 
 vlog_ex = vlog.VerilogExtractor()
-vlog_mods = vlog_ex.extract_objects_from_source('Test_1.v')
+vlog_mods = vlog_ex.extract_objects_from_source('test7.v')
 
-with open('Test_1.v', 'rt') as fh:
+with open('test7.v', 'rt') as fh:
   code = fh.read()
 vlog_mods = vlog_ex.extract_objects_from_source(code)
 
-vlog_mods = vlog_ex.extract_objects('Test_1.v')
+vlog_mods = vlog_ex.extract_objects('test7.v')
 
 for m in vlog_mods:
   print('Module "{}":'.format(m.name))
