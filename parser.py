0da303f65e2e4c0b5f5e5dbbc73e061fce03e1d8
alyzgit <alyz@aucegypt.edu>
2021-10-11 14:14:50 +0200

    update

--- parser.py
+++ parser.py
@@ -1,25 +1,49 @@
 import hdlparse.verilog_parser as vlog
+# generate random integer values
+from random import seed
+from random import randint
+# seed random number generator
+seed(1)
 
-
-
+# HDL Parse Declarations
 vlog_ex = vlog.VerilogExtractor()
-vlog_mods = vlog_ex.extract_objects_from_source('test7.v')
+vlog_mods = vlog_ex.extract_objects_from_source('example.v')
 
-with open('test7.v', 'rt') as fh:
+with open('example.v', 'rt') as fh:
   code = fh.read()
 vlog_mods = vlog_ex.extract_objects_from_source(code)
 
-vlog_mods = vlog_ex.extract_objects('test7.v')
+vlog_mods = vlog_ex.extract_objects('example.v')
 
+
+print ('\n')
+print('`timescale 1ns/1ps')
 for m in vlog_mods:
-  print('Module "{}":'.format(m.name))
-
-  print('  Parameters:')
+  print('Module {}'.format(m.name),'_tb')
+  print('(')
   for p in m.generics:
-    print('\t{:20}{:8}{}'.format(p.name, p.mode, p.data_type))
-
-  print('  Ports:')
+   # f.write('\t{:20}{:8}{}'.format("reg", p.name, p.mode, p.data_type))
+    print('\t{:20}{:8}{},'.format(p.name, p.mode, p.data_type))
+  print(');')
+  
+        
   for p in m.ports:
-    print('\t{:20}{:8}{}'.format(p.name, p.mode, p.data_type))
+    if p.mode== 'input':
+      print('\t{:20}{:8}{}'.format("reg", p.name, p.mode, p.data_type))
+    else:
+      print('\t{:20}{:8}{}'.format("wire", p.name, p.mode, p.data_type))
 
 
+print ('\n')
+with open('testbench.txt', 'w') as f:
+  f.write('Module "{}":'.format(m.name))
+  for m in vlog_mods:
+    
+      f.write('\t{:20}{:8}{}'.format("reg", p.name, p.mode, p.data_type))
+
+# generate some integers
+  print('initial begin')
+  for p in m.generics:
+    print('\t{:20}{:8}{}'.format(p.name, '=', randint(0, 10)))
+  print('$monitor()')
+print('end\n','endmodule')
--- test7.v
+++ test7.v
@@ -1,8 +1,62 @@
-module newstyle // This is a new style module def
-#(parameter real foo = 8, bar=1, baz=2,
-parameter signed [7:0] zip = 100)
-(
-  input x, x2, inout y, y2_long_output,
-  output wire [4:1] z, z2
-);
+timescale 1 ns/10 ps  // time-unit = 1 ns, precision = 10 ps
+
+module half_adder_procedural_tb;
+
+    reg a, b;
+    wire sum, carry;
+
+    // duration for each bit = 20 * timescale = 20 * 1 ns  = 20ns
+    localparam period = 20;  
+
+    half_adder UUT (.a(a), .b(b), .sum(sum), .carry(carry));
+reg clk;
+
+// note that sensitive list is omitted in always block
+// therefore always-block run forever
+// clock period = 2 ns
+always 
+begin
+    clk = 1'b1; 
+    #20; // high for 20 * timescale = 20 ns
+
+    clk = 1'b0;
+    #20; // low for 20 * timescale = 20 ns
+end
+
+always @(posedge clk)
+begin
+    // values for a and b
+    a = 0;
+    b = 0;
+    #period; // wait for period
+    // display message if output not matched
+    if(sum != 0 || carry != 0)  
+        $display("test failed for input combination 00");
+
+    a = 0;
+    b = 1;
+    #period; // wait for period 
+    if(sum != 1 || carry != 0)
+        $display("test failed for input combination 01");
+
+    a = 1;
+    b = 0;
+    #period; // wait for period 
+    if(sum != 1 || carry != 0)
+        $display("test failed for input combination 10");
+
+    a = 1;
+    b = 1;
+    #period; // wait for period 
+    if(sum != 0 || carry != 1)
+        $display("test failed for input combination 11");
+
+    a = 0;
+    b = 1;
+    #period; // wait for period 
+    if(sum != 1 || carry != 1)
+        $display("test failed for input combination 01");
+
+    $stop;   // end of simulation
+end
 endmodule
